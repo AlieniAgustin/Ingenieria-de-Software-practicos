db4ec5e05ffef712dd60f6c5e9a9d6fdba575f5e
AlieniAgustin <alieniagustinweb@gmail.com>
2025-09-14 11:55:13 -0300

    add tp3

---
+++ tp3/ej1/practicos-ingenieria-25/3-observer/weather-station/src/main/java/CelsiusDisplay.java
@@ -0,0 +1,17 @@
+public class CelsiusDisplay implements Observer, DisplayElement{
+  private float celsiusTemp;
+  private WeatherData weatherData;
+
+  public CelsiusDisplay(WeatherData weatherData){ 
+    this.weatherData = weatherData;
+  }
+
+  public void update(float temp, float humidity, float pressure){
+    celsiusTemp = (temp - 32) * (5.0f/9);
+    display();
+  }
+
+  public void display(){
+    System.out.println(celsiusTemp + " C degrees.");
+  }
+}
